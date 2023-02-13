f55969b641be209c8e95b859a1c01105b332aed3
suraj <surajmishra9318@gmail.com>
2023-02-13 20:53:24 +0530

    LoginDoneWithJWT

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const UserModel = require('./models/user')
 const connectDB = require('./db/connection')
-
-
+const bcrypt = require('bcrypt');
+const salt = bcrypt.genSaltSync(10);
+const secret = 'sklfslkfjslflkjsalkfalfkjsalfk'
+const jwt = require('jsonwebtoken')
 
 const app = express();
 app.use(cors())
@@ -13,12 +15,41 @@ app.use(express.json())
 
 app.post('/register',async(req,res)=>{
 try {
-    const request = await UserModel.create(req.body);
-    res.status(201).json(request);
+    const {username, password} = req.body;
+    console.log(username, password)
+    const request = await UserModel.create({username, 
+        password : bcrypt.hashSync(password,salt)});
+        res.status(201).json(request);
 } catch (error) {
     res.status(500).json({msg : 'something went wrong'});
 }
 })
+
+
+app.post('/login',async(req,res)=>{
+    const {username,password} = req.body;
+    const request = await UserModel.findOne({username})
+   if(request !== null){
+    const passOk =  bcrypt.compareSync(password ,request.password)
+    if(passOk){
+        //login
+        jwt.sign({username, id : request._id}, secret , {}, (err,token)=>{
+            if( err) throw err;
+            res.cookie('token',token).json({msg : 'valid user'})
+        })
+    }
+    else{
+        res.status(200).json({msg : "Wrong Credentials"});
+    }
+   }
+   else{
+        res.status(200).json({msg : "Wrong Credentials"});
+   }
+})
+
+
+
+
 
 const port = 5000;
 
